cd66e622f7a159ac986777117dd7da972f78d91d
Davit-Meliqyan <76562656+Davit-Meliqyan@users.noreply.github.com>
2021-01-05 16:51:54 +0400

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void main(String [] args){
+        Scanner scanner =new Scanner(System.in);
+        int k=scanner.nextInt();
+        int[] number = new int[k];
+        for(int i=0 ; i<number.length ; i++){
+            number[i] = scanner.nextInt();
+        }
+        for(int i = number.length-1 ; i > 0 ; i--){
+            for(int j = 0 ; j < i ; j++){
+
+                if( number[j] > number[j+1] ){
+                    int tmp = number[j];
+                    number[j] = number[j+1];
+                    number[j+1] = tmp;
+                }
+            }
+        }
+        for(int i=0 ; i<number.length ; i++){
+            // numberMax[i]=max;
+            System.out.print(number[i] + " ");
+        }
+    }
+}
+
